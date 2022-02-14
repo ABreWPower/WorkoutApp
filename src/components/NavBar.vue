054feffb637f804cebd9fc7d75f3a326d887c062
Patrick Shafer <patrick.shafer@gmail.com>
2022-02-14 01:50:19 +0000

    Fixed overlapping slide-out problem

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,35 +1,45 @@
 <template id="NavBar">
   <div>
-  <v-ons-splitter>
-    <v-ons-splitter-side swipeable width="150px" collapse="" side="right" :open.sync="openSide">
-      <v-ons-page>
-        <v-ons-list>
-          <v-ons-list-item v-for="page in pages" tappable modifier="chevron" :key="page">
-            <div class="center">{{ page }}</div>
-          </v-ons-list-item>
-        </v-ons-list>
-      </v-ons-page>
-    </v-ons-splitter-side>
+    <v-ons-splitter>
+      <v-ons-splitter-side
+        swipeable
+        width="150px"
+        collapse=""
+        side="right"
+        :open.sync="openSide"
+      >
+        <v-ons-page>
+          <v-ons-list>
+            <v-ons-list-item
+              v-for="page in pages"
+              tappable
+              modifier="chevron"
+              :key="page"
+            >
+              <div class="center">{{ page }}</div>
+            </v-ons-list-item>
+          </v-ons-list>
+        </v-ons-page>
+      </v-ons-splitter-side>
 
-    <v-ons-splitter-content>
-      {{ page }}
-      <!-- <component :is="currentPage" :toggle-menu="() => (openSide = !openSide)"></component> -->
-    </v-ons-splitter-content>
-  </v-ons-splitter>
-
-  <v-ons-toolbar>
-    <div class="left">
-      <v-ons-toolbar-button>
-        <v-ons-icon icon="md-arrow-left"></v-ons-icon>
-      </v-ons-toolbar-button>
-    </div>
-    <div class="center">{{ title }}</div>
-    <div class="right">
-      <v-ons-toolbar-button>
-        <v-ons-icon icon="md-menu" @click="menuAction()"></v-ons-icon>
-      </v-ons-toolbar-button>
-    </div>
-  </v-ons-toolbar>
+      <v-ons-splitter-content>
+        <v-ons-toolbar>
+          <div class="left">
+            <v-ons-toolbar-button>
+              <v-ons-icon icon="md-arrow-left"></v-ons-icon>
+            </v-ons-toolbar-button>
+          </div>
+          <div class="center">{{ title }}</div>
+          <div class="right">
+            <v-ons-toolbar-button>
+              <v-ons-icon icon="md-menu" @click="menuAction()"></v-ons-icon>
+            </v-ons-toolbar-button>
+          </div>
+        </v-ons-toolbar>
+        <p style="text-align: center">Welcome ass hat.</p>
+        <!-- <component :is="currentPage" :toggle-menu="() => (openSide = !openSide)"></component> -->
+      </v-ons-splitter-content>
+    </v-ons-splitter>
   </div>
 </template>
 
@@ -37,19 +47,20 @@
 export default {
   name: "NavBar",
   props: {
-    title: String
+    title: String,
   },
-  data: function() {
+  data: function () {
     return {
+      page: "Page one",
       pages: ["Page one", "Page two", "Page three"],
-      openSide: false
+      openSide: false,
     }
   },
   methods: {
-    menuAction: function() {
-      this.openSide = !this.openSide
-    }
-  }
+    menuAction: function () {
+      this.openSide = !this.openSide;
+    },
+  },
 };
 </script>
 
