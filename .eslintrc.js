33400bb9f7e0af2c0e4b722a34394ebfdfcf2f4f
Patrick Shafer <patrick.shafer@gmail.com>
2022-02-14 02:41:50 +0000

    Added space-before-function-paren: nevah!

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //fix spaces before parens in functions
+    'space-before-function-paren': ['error', 'never']
   }
 }
