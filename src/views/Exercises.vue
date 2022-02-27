864ac70367642ad7f9eaf35f99f214a91f6c39ba
Adam Wickersham <abwicker@gmail.com>
2022-02-27 20:10:55 +0000

    updated exercises to match workouts chagnes

--- src/views/Exercises.vue
+++ src/views/Exercises.vue
@@ -49,6 +49,42 @@ const exercises = {
     reps: 15,
     duration: null,
     equipment: []
+  },
+  5: {
+    id: 4,
+    name: 'Exercise 5',
+    picture: 'TODO Something',
+    video: 'TODO Something',
+    instructions: 'Crunches on the floor',
+    difficulty: 1,
+    muscleGroup: ['abs'],
+    reps: 15,
+    duration: null,
+    equipment: []
+  },
+  6: {
+    id: 4,
+    name: 'Exercise 5',
+    picture: 'TODO Something',
+    video: 'TODO Something',
+    instructions: 'Crunches on the floor',
+    difficulty: 1,
+    muscleGroup: ['abs'],
+    reps: 15,
+    duration: null,
+    equipment: []
+  },
+  7: {
+    id: 4,
+    name: 'Exercise 5',
+    picture: 'TODO Something',
+    video: 'TODO Something',
+    instructions: 'Crunches on the floor',
+    difficulty: 1,
+    muscleGroup: ['abs'],
+    reps: 15,
+    duration: null,
+    equipment: []
   }
 }
 </script>
@@ -58,11 +94,17 @@ const exercises = {
     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
     <button class="btn btn-outline-success" type="submit">Search</button>
   </form>
-  <div v-for="exercise in exercises" :key="exercise">
-    <card-view :name="exercise.name" :picture="exercise.picture" :video="exercise.video" :instructions="exercise.instructions"></card-view>
+  <!-- height: calc(100% vertical height - Nav bar (56px) - Search bar (38px) - New exercise btn (38px) - IDK-DIK (16px) - Card margin (0.5rem) - Toolbar margin (0.5rem) -->
+  <div style="position: relative; height: calc(100vh - 56px - 38px - 38px - 16px - 0.5rem - 0.5rem); margin-top: 0.5rem; overflow: auto">
+    <div v-for="exercise in exercises" :key="exercise">
+      <card-view :name="exercise.name" :picture="exercise.picture" :video="exercise.video" :description="exercise.instructions"></card-view>
+    </div>
   </div>
   <button type="button" class="btn btn-outline-secondary">New Exercise</button>
 </template>
 
 <style scoped>
+div.card-view:last-child div.card {
+  margin-bottom: 0 !important;
+}
 </style>
