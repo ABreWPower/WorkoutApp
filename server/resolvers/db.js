ba60d301231995f23655c75a8672aef2c71c6b3f
Adam Wickersham <abwicker@gmail.com>
2022-03-23 15:50:24 +0000

    working on resolvers

---
+++ server/resolvers/db.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql')
+require('dotenv').config()
+
+const connectSQLDB = function () {
+  console.log("connectSQLDB()...")
+  let retVal = mysql.createConnection({
+    host: process.env.HOST,
+    port: 3306,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB
+  })
+  retVal.on('error', function (err) {
+    console.log("It's dead, Jim...", err)
+  })
+  return retVal
+}
+
+const querySQLDB = sql => {
+  let connection = connectSQLDB()
+  return new Promise((resolve, reject) => connection.query(sql, (error, results, fields) => {
+    console.log('querySQLDB error', error)
+    console.log('querySQLDB results', results)
+    console.log('querySQLDB fields', fields)
+    if (error) reject(error)
+    else resolve(results)
+  }))
+}
--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,68 +1,91 @@
-const mysql = require('mysql')
+require('./db.js')
 require('dotenv').config()
 
-const connectSQLDB = function () {
-  console.log("connectSQLDB()...")
-  let retVal = mysql.createConnection({
-    host: process.env.HOST,
-    port: 3306,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB
-  })
-  retVal.on('error', function (err) {
-    console.log("It's dead, Jim...", err)
-  })
-  return retVal
-}
-
-const querySQLDB = sql => {
-  let connection = connectSQLDB()
-  return new Promise((resolve, reject) => connection.query(sql, (error, results, fields) => {
-    console.log('querySQLDB error', error)
-    console.log('querySQLDB results', results)
-    console.log('querySQLDB fields', fields)
-    if (error) reject(error)
-    else resolve(results)
-  }))
-}
+// TODO make a resolver and mutation that sets up the database for initial testing
+// TODO make one that clears tables to clean up
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => querySQLDB("SELECT * FROM book").then(result => result),
-    numberSix: () => "6",
-    dateTime: () => querySQLDB("SELECT UTC_TIMESTAMP").then(result => String(result[0]['UTC_TIMESTAMP'])),
-    authors: () => querySQLDB("SELECT * FROM author").then(result => result),
+    users: () => querySQLDB("SELECT * FROM user").then(result => result),
+    workouts: () => querySQLDB("SELECT * FROM workout").then(result => result),
+    equipment: () => querySQLDB("SELECT * FROM equipment").then(result => result),
+    musclegroups: () => querySQLDB("SELECT * FROM musclegroup").then(result => result),
+    exercises: () => querySQLDB("SELECT * FROM exercise").then(result => result),
+    workoutlogs: () => querySQLDB("SELECT * FROM workoutlog").then(result => result),
+    workoutlogexercises: () => querySQLDB("SELECT * FROM workoutlogexercise").then(result => result),
   },
-  Book: {
-    Authors(parent) {
-      return querySQLDB({ sql: "SELECT author.* FROM author_book INNER JOIN author ON author_book.authorID = author.ID WHERE author_book.bookID = ?", values: parent.ID }).then(result => result)
-    }
-  },
-  Author: {
-    Books(parent) {
-      return querySQLDB({ sql: "SELECT book.* FROM author_book INNER JOIN book ON author_book.bookID = book.ID WHERE author_book.authorID = ?", values: parent.ID }).then(result => result)
-    }
-  },
-  Mutation: {
-    addBook: (parent, { title }) => {
-      return querySQLDB({ sql: "INSERT into book (Title) VALUES (?)", values: title }).then(
-        result => querySQLDB({ sql: "SELECT * FROM book WHERE ID = ?", values: result.insertId }).then(result => result[0])
-      )
+  Workout: {
+    user(parent) {
+      return querySQLDB({ sql: "SELECT * FROM user WHERE ID = ?", values: parent.ID }).then(result => result)
     },
-    addAuthor: (parent, { name }) => {
-      return querySQLDB({ sql: "INSERT into author (Name) VALUES (?)", values: name }).then(
-        result => querySQLDB({ sql: "SELECT * FROM author WHERE ID = ?", values: result.insertId }).then(result => result[0])
-      )
+    equipment(parent) {
+      return querySQLDB({ sql: "SELECT * FROM equipment WHERE ID = ?", values: parent.ID }).then(result => result)
     },
-    addAuthorToBook: (parent, { bookID, authorID }) => {
-      return querySQLDB({ sql: "INSERT into author_book (bookID, authorID) VALUES (?, ?)", values: [bookID, authorID] }).then(
-        result => querySQLDB({ sql: "SELECT * FROM book WHERE ID = ?", values: bookID }).then(result => result[0])
-      )
+    exercises(parent) {
+      return querySQLDB({ sql: "SELECT * FROM exercise WHERE ID = ?", values: parent.ID }).then(result => result)
     }
-  }
+  },
+  Exercise: {
+    musclegroups(parent) {
+      return querySQLDB({ sql: "SELECT * FROM musclegroup WHERE ID = ?", values: parent.ID }).then(result => result)
+    },
+    equipment(parent) {
+      return querySQLDB({ sql: "SELECT * FROM equipment WHERE ID = ?", values: parent.ID }).then(result => result)
+    }
+  },
+  WorkoutLog: {
+    user(parent) {
+      return querySQLDB({ sql: "SELECT * FROM user WHERE ID = ?", values: parent.ID }).then(result => result)
+    },
+    workout(parent) {
+      return querySQLDB({ sql: "SELECT * FROM workout WHERE ID = ?", values: parent.ID }).then(result => result)
+    },
+    wlexercises(parent) {
+      return querySQLDB({ sql: "SELECT * FROM workoutlogexercise WHERE ID = ?", values: parent.ID }).then(result => result)
+    }
+  },
+  WorkoutLogExercise: {
+    workoutlog(parent) {
+      return querySQLDB({ sql: "SELECT * FROM workoutlog WHERE ID = ?", values: parent.ID }).then(result => result)
+    },
+    exercise(parent) {
+      return querySQLDB({ sql: "SELECT * FROM exercise WHERE ID = ?", values: parent.ID }).then(result => result)
+    }
+  },
+  // Mutation: {
+  // }
+
+
+  
+  // Book: {
+  //   Authors(parent) {
+  //     return querySQLDB({ sql: "SELECT author.* FROM author_book INNER JOIN author ON author_book.authorID = author.ID WHERE author_book.bookID = ?", values: parent.ID }).then(result => result)
+  //   }
+  // },
+  // Author: {
+  //   Books(parent) {
+  //     return querySQLDB({ sql: "SELECT book.* FROM author_book INNER JOIN book ON author_book.bookID = book.ID WHERE author_book.authorID = ?", values: parent.ID }).then(result => result)
+  //   }
+  // },
+  // Mutation: {
+  //   addBook: (parent, { title }) => {
+  //     return querySQLDB({ sql: "INSERT into book (Title) VALUES (?)", values: title }).then(
+  //       result => querySQLDB({ sql: "SELECT * FROM book WHERE ID = ?", values: result.insertId }).then(result => result[0])
+  //     )
+  //   },
+  //   addAuthor: (parent, { name }) => {
+  //     return querySQLDB({ sql: "INSERT into author (Name) VALUES (?)", values: name }).then(
+  //       result => querySQLDB({ sql: "SELECT * FROM author WHERE ID = ?", values: result.insertId }).then(result => result[0])
+  //     )
+  //   },
+  //   addAuthorToBook: (parent, { bookID, authorID }) => {
+  //     return querySQLDB({ sql: "INSERT into author_book (bookID, authorID) VALUES (?, ?)", values: [bookID, authorID] }).then(
+  //       result => querySQLDB({ sql: "SELECT * FROM book WHERE ID = ?", values: bookID }).then(result => result[0])
+  //     )
+  //   }
+  // }
 }
 
 exports.resolvers = resolvers
