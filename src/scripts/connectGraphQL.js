6a319fb03ab5ae54fa78e3afdad25eaab2f7e926
Adam Wickersham <abwicker@gmail.com>
2022-03-28 20:01:46 +0000

    created a graphql interface script

---
+++ src/scripts/connectGraphQL.js
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
+import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
+
+// Setup apollo client:
+const cache = new InMemoryCache()
+export let forceNetworkJQL = false
+
+persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage)
+})
+
+export const client = new ApolloClient({
+  uri: "https://5ipdns9qfj.execute-api.us-east-1.amazonaws.com/dev/",
+  cache: cache
+});
--- src/views/Workouts.vue
+++ src/views/Workouts.vue
@@ -1,25 +1,8 @@
 <script setup>
 import cardView from '../components/CardView.vue'
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
-import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
-import  { ref } from "vue"
-
-// Setup apollo client:
-const cache = new InMemoryCache()
-let forceNetworkJQL = false
-
-persistCache({
-  cache,
-  storage: new LocalStorageWrapper(window.localStorage)
-})
-
-const client = new ApolloClient({
-  uri: "https://5ipdns9qfj.execute-api.us-east-1.amazonaws.com/dev/",
-  cache: cache,
-  // headers: {
-  //   "x-api-key": "da2-t4ee7yn64raenc4ntec4vusvpi",
-  // },
-});
+import { client, forceNetworkJQL } from  "../scripts/connectGraphQL.js"
+import { gql } from "@apollo/client/core";
+import { ref } from "vue"
 
 let getWorkouts = gql`
   query Query {
@@ -54,79 +37,6 @@ client.query({
   console.log("workouts", workouts)
 })
 
-
-// const workouts = {
-//   1: {
-//     id: 1,
-//     name: 'Workout 1',
-//     picture: 'TODO Something',
-//     description: 'A new workout',
-//     user: 'default',
-//     duration: 1800,
-//     difficulty: 1,
-//     equipment: ['barbell', 'dip machine']
-//   },
-//   2: {
-//     id: 2,
-//     name: 'Workout B',
-//     picture: 'TODO Something',
-//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius tempor justo, ac cursus urna rhoncus id. Etiam id hendrerit metus, et eleifend libero. Phasellus volutpat, erat sed dignissim dignissim, tellus metus elementum mi, vitae vulputate ipsum orci a felis. Phasellus justo justo, tincidunt ac nibh suscipit, iaculis luctus nisi. Suspendisse fringilla dictum viverra. Nullam a pulvinar nisl. Fusce mattis, diam et pretium semper, ante lacus convallis diam, iaculis scelerisque massa tellus a dui. Aliquam eu leo sed metus vehicula finibus. Nam finibus, neque eget congue fringilla, turpis nisl lacinia arcu, eu porttitor tortor tellus sit amet eros. Etiam risus orci, suscipit a justo nec, suscipit placerat est. Nam imperdiet placerat aliquam.',
-//     user: 'default',
-//     duration: 300,
-//     difficulty: 2,
-//     equipment: ['barbell', 'dumbell']
-//   },
-//   3: {
-//     id: 3,
-//     name: 'Workout 3C',
-//     picture: 'TODO Something',
-//     description: 'A crazy workout',
-//     user: 'default',
-//     duration: 1200,
-//     difficulty: 1,
-//     equipment: ['barbell']
-//   },
-//   4: {
-//     id: 4,
-//     name: 'Workout 5',
-//     picture: 'TODO Something',
-//     description: 'IDK',
-//     user: 'default',
-//     duration: 700,
-//     difficulty: 3,
-//     equipment: ['barbell', 'dumbell', 'dip machine']
-//   },
-//   5:{
-//     id: 4,
-//     name: 'Workout 5',
-//     picture: 'TODO Something',
-//     description: 'IDK',
-//     user: 'default',
-//     duration: 700,
-//     difficulty: 3,
-//     equipment: ['barbell', 'dumbell', 'dip machine']
-//   },
-//   6:{
-//     id: 4,
-//     name: 'Workout 5',
-//     picture: 'TODO Something',
-//     description: 'IDK',
-//     user: 'default',
-//     duration: 700,
-//     difficulty: 3,
-//     equipment: ['barbell', 'dumbell', 'dip machine']
-//   },
-//   7:{
-//     id: 4,
-//     name: 'Workout 5',
-//     picture: 'TODO Something',
-//     description: 'IDK',
-//     user: 'default',
-//     duration: 700,
-//     difficulty: 3,
-//     equipment: ['barbell', 'dumbell', 'dip machine']
-//   }
-// }
 </script>
 
 <template>
