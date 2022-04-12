3d26d8e20d3a454273dc9a8673dfcb7920c97822
Adam Wickersham <abwicker@gmail.com>
2022-04-12 23:36:47 +0000

    fixed the problems in exervise view

--- src/views/ExerciseView.vue
+++ src/views/ExerciseView.vue
@@ -5,6 +5,15 @@ import { client, forceNetworkJQL } from  "../scripts/connectGraphQL.js"
 import { gql } from "@apollo/client/core";
 import { ref } from "vue"
 
+import Workout1Pic from "/pic1.jpg"
+import Workout2Pic from "/pic2.jpg"
+import Workout3Pic from "/pic3.jpg"
+
+const getImages = function () {
+  let images = [Workout1Pic, Workout2Pic, Workout3Pic];
+  return images[Math.floor(Math.random() * images.length)];
+}
+
 const routeObj = useRoute()
 console.log("exerciseid from router", routeObj.params.exerciseid)
 
@@ -61,9 +70,12 @@ client.query({
 <template>
   <h1>{{ exercise.name }}</h1>
   <!-- <img src="/pic1.jpg" alt="pic1" style="max-width: 100vw" /> -->
+  <img v-bind:src="getImages(picture)" v-bind:alt="name" class="img-fluid rounded-start w-100" style="" />
 
   <div class="row align-items-start">
-    <div class="col">Duration: {{ exercise.duration / 60 }} hours</div>
+    <div class="col">
+      <p>Muscle Group: {{ musclegroups }}</p>
+    </div>
     <div class="col">
       <div class="row align-items-start">
         <p>
@@ -77,25 +89,32 @@ client.query({
     </div>
   </div>
 
-  <p>Required Equipment: {{ equipment }}</p>
+  <div class="row align-items-start">
+    <div class="col">
+      <p>Duration: {{ exercise.duration }} minutes</p>
+    </div>
+    <div class="col">
+      <p>Required Equipment: {{ equipment }}</p>
+    </div>
+  </div>
 
   <h4>Description</h4>
   <p>
     {{ exercise.instructions }}
   </p>
 
-  <div v-for="exercise in workout.exercises" :key="exercise">
+  <!-- <div v-for="exercise in workout.exercises" :key="exercise">
     <card-view :name="exercise.name" :picture="exercise.picture" :video="exercise.video" :description="exercise.instructions" :exerciseid="exercise.id"></card-view>
-  </div>
+  </div> -->
 
-  <div class="row">
+  <!-- <div class="row">
     <div class="col">
       <button type="button" class="btn btn-primary">Start</button>
-    </div>
-    <div class="col">
-      <button type="button" class="btn btn-outline-secondary">Edit</button>
-    </div>
-  </div>
+    </div> 
+    <div class="col"> -->
+  <button type="button" class="btn btn-outline-secondary">Edit</button>
+  <!-- </div>
+  </div> -->
 </template>
 
 <style scoped>
