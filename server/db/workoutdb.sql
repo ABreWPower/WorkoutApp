ba8924af643c227fbf751988d9e1e985b3bc34d6
Patrick Shafer <patrick.shafer@gmail.com>
2022-04-21 02:16:42 +0000

    Updated workoutapp.sql with latest schema

--- server/db/workoutdb.sql
+++ server/db/workoutdb.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
 --
--- Host: devbox.home.lan    Database: workoutapp
+-- Host: localhost    Database: workoutapp
 -- ------------------------------------------------------
 -- Server version	8.0.28-0ubuntu0.21.10.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -299,4 +299,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-20 21:24:46
+-- Dump completed on 2022-04-21  2:12:45
