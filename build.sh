0fa9b6f329fa8b00ebc41ec8db359b204aad7a36
Adam Wickersham <abwicker@gmail.com>
2022-05-13 16:07:44 +0000

    fixing build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ DIR=".serverless"
 if [ -d "$DIR" ]; then
   ### Take action if $DIR exists ###
   echo "Serverless already deployed, only pushing up package"
-  serverless deploy function -f graphql
+  serverless deploy function -f workoutapp
 else
   ###  Control will jump here if $DIR does NOT exists ###
   echo "Serverless not deployed yet, deploy the whole thing"
