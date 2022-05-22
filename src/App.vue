6748e410d26af318249228d9560878f4113e345b
Adam Wickersham <abwicker@gmail.com>
2022-05-22 22:19:32 +0000

    Nav bar now closes when you click one of the routes

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,31 @@ watch(routeObj, () => {
   // console.log(routeObj.name)
   pageTitle.value = routeObj.name
 })
+
+// Click handler for opening and closing nav bar
+let navBarToggle = false
+function navBarButtonClick() {
+  let navBar = document.getElementById("navbarSupportedContent")
+  if (navBarToggle == false) {
+    navBar.style.display = "block"
+    navBarToggle = true
+  }
+  else {
+    navBar.style.display = "none"
+    navBarToggle = false
+  }
+}
+
+// Close the nav bar on routing
+function closeNavBar() {
+  let navBar = document.getElementById("navbarSupportedContent")
+  navBar.style.display = "none"
+  navBarToggle = false
+}
 </script>
 
 <template>
   <!-- TODO close dropdown menu after a selection -->
-  <!-- TODO need to get the back button to match the rest of the nav bar -->
   <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary mb-2" style="color: white; padding-left: 20px; padding-right 20px;">
     <div class="container-fluid">
       <div class="mx-3">
@@ -20,9 +40,9 @@ watch(routeObj, () => {
           <i class="bi bi-arrow-left"></i>
         </button>
       </div>
-      <!-- <a class="navbar-brand" href="#">{{ pageTitle }}</a> -->
       <h2>{{ pageTitle }}</h2>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
+      <button class="navbar-toggler" type="button" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" @click="navBarButtonClick()">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -31,10 +51,10 @@ watch(routeObj, () => {
             <p style="color: white; padding-left: 20px; padding-right 20px;">Workout App</p>
           </li>
           <!-- data-bs-toggle="collapse" data-bs-target=".navbar-collapse"  broke the links -->
-          <li class="nav-item">
+          <li class="nav-item" @click="closeNavBar()">
             <router-link to="/" style="color: white; padding-left: 20px; padding-right 20px;">Workouts</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" @click="closeNavBar()">
             <router-link to="/exercises" style="color: white; padding-left: 20px; padding-right 20px;">Exercises</router-link>
           </li>
         </ul>
