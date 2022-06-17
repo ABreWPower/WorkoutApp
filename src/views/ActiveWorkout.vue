b9399ca66e3c1864ad3432fbd9890089d2e7e713
Adam Wickersham <abwicker@gmail.com>
2022-06-17 00:50:44 +0000

    updated active workout to pull back an all of the data needed to run the page

--- src/views/ActiveWorkout.vue
+++ src/views/ActiveWorkout.vue
@@ -8,10 +8,53 @@ import router from "../router/router.js"
 const routeObj = useRoute()
 console.log("workout edit router params", routeObj.params)
 
+let getActiveWorkoutData = gql`
+  query Query($workoutlogsId: ID) {
+    workoutlogs(id: $workoutlogsId) {
+      id
+      exerciselogs {
+        id
+        sets
+        reps
+        duration
+        rest
+        weight
+        exercise {
+          id
+          name
+          picture
+          video
+          instructions
+          equipment {
+            name
+            icon
+          }
+        }
+      }
+    }
+  }
+`
+
+var somethingcool = null
+var exerciseName = ref()
+
+client.query({
+  query: getActiveWorkoutData,
+  variables: { workoutlogsId: parseInt(routeObj.params.workoutlogid)},
+  fetchPolicy: forceNetworkJQL ? 'network-only' : 'cache-first'
+})
+.then(result => {
+  console.log("results", result)
+  somethingcool = result.data.workoutlogs[0]
+  exerciseName.value = somethingcool.exerciselogs[0].exercise.name
+})
+
+
+
 </script>
 
 <template>
-  <h1>TODO Exercise Name</h1>
+  <h1>Here {{ exerciseName }}</h1>
   <p>Equipment: TODO Equipment</p>
 
   <!-- TODO Split the rest of the space into 3 equal parts -->
@@ -24,6 +67,7 @@ console.log("workout edit router params", routeObj.params)
   <div class="row">
     <!-- TODO Rep stuff -->
     <button type="button" class="btn btn-success btn-lg">Continue</button>
+    <!-- TODO make the button not cross the whole screen -->
   </div>
 
   <nav class="navbar fixed-bottom navbar-dark bg-primary" style="color: white; padding-left: 20px; padding-right 20px;">
