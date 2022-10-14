faabf4931ad801c207ab21acf82475bf7195e8cb
Patrick Shafer <patrick.shafer@gmail.com>
2022-10-14 00:26:07 +0000

    Fixed? or worked around? a bug where the same exercise in a workout didn't display different reps/durations/rest

--- src/scripts/connectGraphQL.js
+++ src/scripts/connectGraphQL.js
@@ -2,11 +2,17 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
 import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
 
 // Setup apollo client:
-const cache = new InMemoryCache()
+const cache = new InMemoryCache({
+  typePolicies: {
+    Exercise: {
+      keyFields: ["id", "reps", "duration", "rest"]
+    }
+  }
+})
 export let forceNetworkJQL = true
 
 let apolloUri = `http://${window.location.hostname}:4000/`
-if(window.location.hostname === "workoutapp.adamwickersham.com") {
+if (window.location.hostname === "workoutapp.adamwickersham.com") {
   apolloUri = "https://5ipdns9qfj.execute-api.us-east-1.amazonaws.com/dev/"
 }
 
